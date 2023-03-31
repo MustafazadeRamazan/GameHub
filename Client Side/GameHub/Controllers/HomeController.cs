d6e50bf593ab51ffae2953c2cddc46c48d4e913f
MustafazadeRamazan <workman24680@gmail.com>
2023-04-01 01:25:47 +0400

    Added Products, Images, Fixed Bugs,Optimized Code

--- Client Side/GameHub/Controllers/HomeController.cs
+++ Client Side/GameHub/Controllers/HomeController.cs
@@ -15,10 +15,11 @@ namespace GameHub.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            ViewBag.MenProduct = db.Products.Where(x => x.Category.Name.Equals("Game Accounts")).ToList();
-            ViewBag.WomenProduct = db.Products.Where(x => x.Category.Name.Equals("Game Projects")).ToList();
-            ViewBag.SportsProduct = db.Products.Where(x => x.Category.Name.Equals("Laptops")).ToList();
-            ViewBag.ElectronicsProduct = db.Products.Where(x => x.Category.Name.Equals("Gaming Accessories")).ToList();
+            ViewBag.FpsGames = db.Products.Where(x => x.Category.Name.Equals("Fps Games")).ToList();
+            ViewBag.GameProjects = db.Products.Where(x => x.Category.Name.Equals("Game Projects")).ToList();
+            ViewBag.SportsProduct = db.Products.Where(x => x.Category.Name.Equals("Sport Games")).ToList();
+            ViewBag.ActionAdventure = db.Products.Where(x => x.Category.Name.Equals("Action Adventure")).ToList();
+            ViewBag.SurvivalGames = db.Products.Where(x => x.Category.Name.Equals("Survival Games")).ToList();
             ViewBag.Slider = db.genMainSliders.ToList();
             ViewBag.PromoRight = db.genPromoRights.ToList();
 
