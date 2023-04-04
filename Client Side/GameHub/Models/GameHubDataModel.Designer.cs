3f9feb77765d55d1d2fd68398ebff96c1de6f236
MustafazadeRamazan <workman24680@gmail.com>
2023-04-05 00:49:51 +0400

    Added Quantity, Added Dollar Icon, Updated Footer

--- Client Side/GameHub/Models/GameHubDataModel.Designer.cs
+++ Client Side/GameHub/Models/GameHubDataModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Workm\Desktop\GameHub\Client Side\Kahreedo\Models\GameHubDataModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Workm\Desktop\GameHub\Client Side\GameHub\Models\GameHubDataModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
